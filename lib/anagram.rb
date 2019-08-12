1af6248fcbe302fdd6fe964c0266414b42a331a7
Darin Webster <darinleenavy@gmail.com>
2019-08-12 22:30:30 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,26 +15,4 @@ class Anagram
     end
     output
   end
-  
 end
-
-
-# class Anagram
-
-#   attr_accessor :word
-
-#   def initialize(word)
-#     @word = word
-#   end
-
-#   def match(collection)
-#     output = []
-#     collection.each_with_index do |el, idx|
-#       if el.split("").sort.join == word.split("").sort.join
-#         output << collection[idx]
-#       end
-#     end
-#     output
-#   end
-
-# end
